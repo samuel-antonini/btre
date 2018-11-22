519250adfdb064d2b19850b37eb68d8812995c9b
Samuel Antonini <santonini@gmail.com>
2018-11-22 12:44:03 -0200

    Pass dynamic content to Index and About

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
+from realtors.models import Realtor
+
 
 def index(request):
-    return render(request, 'pages/index.html')
+    # get the last 3 published listings
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    
+    # context for rendering    
+    context = {
+        'listings': listings
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # get all realtors
+    realtors = Realtor.objects.all()
+    
+    # get only realtors that are mvp
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+    
+    # context for rendering
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+    return render(request, 'pages/about.html', context)
